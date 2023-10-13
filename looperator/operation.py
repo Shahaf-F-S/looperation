3484d2c79057a3fdd99bf540eb2c3cf73694182e
Shahaf Frank-Shapir <67577414+Shahaf-F-S@users.noreply.github.com>
2023-10-13 11:56:14 +0300

    Project Update

--- looperator/operation.py
+++ looperator/operation.py
@@ -1,6 +1,9 @@
 # operation.py
 
-from typing import Optional, Tuple, Generic, Any, Dict, TypeVar
+from typing import (
+    Optional, Tuple, Generic, Any,
+    Dict, TypeVar, ClassVar, Union
+)
 
 from attrs import define, field
 
@@ -14,6 +17,8 @@ __all__ = [
     "Operation"
 ]
 
+InputsData = Dict[str, Union[Tuple, Dict[str, Any]]]
+
 @represent
 @define(repr=False, frozen=True)
 class Inputs:
@@ -21,9 +26,50 @@ class Inputs:
 
     args: Optional[Tuple] = field(factory=tuple)
     kwargs: Optional[Dict[str, Any]] = field(factory=dict)
+
+    ARGS: ClassVar[str] = "args"
+    KWARGS: ClassVar[str] = "kwargs"
+
+    try:
+        from typing import Self
+
+    except ImportError:
+        Self = Any
+    # end try
+
+    @classmethod
+    def load(cls, data: InputsData) -> Self:
+        """
+        Creates an instance of the class for the data.
+
+        :param data: The data to load into an object.
+
+        :return: The new instance with the data.
+        """
+
+        return cls(
+            args=data[cls.ARGS],
+            kwargs=data[cls.KWARGS]
+        )
+    # end load
+
+    def json(self) -> InputsData:
+        """
+        Returns a json object to represent the data of the object.
+
+        :return: The data of the object.
+        """
+
+        return {
+            self.ARGS: self.args,
+            self.KWARGS: self.kwargs
+        }
+    # end json
 # end BaseCallbackEvent
 
 _O = TypeVar("_O")
+
+OutputsData = Dict[str, _O]
 
 @represent
 @define(repr=False, frozen=True)
@@ -31,7 +77,42 @@ class Outputs(Generic[_O]):
     """A class to represent an arbitrage event."""
 
     returns: Optional[_O] = None
+
+    RETURNS: ClassVar[str] = "returns"
+
+    try:
+        from typing import Self
+
+    except ImportError:
+        Self = Any
+    # end try
+
+    @classmethod
+    def load(cls, data: OutputsData) -> Self:
+        """
+        Creates an instance of the class for the data.
+
+        :param data: The data to load into an object.
+
+        :return: The new instance with the data.
+        """
+
+        return cls(returns=data[cls.RETURNS])
+    # end load
+
+    def json(self) -> OutputsData:
+        """
+        Returns a json object to represent the data of the object.
+
+        :return: The data of the object.
+        """
+
+        return {self.RETURNS: self.returns}
+    # end json
 # end Outputs
+
+TimeData = Dict[str, float]
+OperationData = Dict[str, Union[InputsData, OutputsData, TimeData]]
 
 @represent
 @define(repr=False, frozen=True)
@@ -41,4 +122,46 @@ class Operation(Generic[_O]):
     time: ProcessTime
     inputs: Inputs
     outputs: Outputs[_O]
+
+    TIME: ClassVar[str] = "time"
+    INPUTS: ClassVar[str] = "inputs"
+    OUTPUTS: ClassVar[str] = "outputs"
+
+    try:
+        from typing import Self
+
+    except ImportError:
+        Self = Any
+    # end try
+
+    @classmethod
+    def load(cls, data: OperationData) -> Self:
+        """
+        Creates an instance of the class for the data.
+
+        :param data: The data to load into an object.
+
+        :return: The new instance with the data.
+        """
+
+        return cls(
+            time=ProcessTime.load(data[cls.TIME]),
+            inputs=Inputs.load(data[cls.INPUTS]),
+            outputs=Outputs.load(data[cls.OUTPUTS])
+        )
+    # end load
+
+    def json(self) -> OperationData:
+        """
+        Returns a json object to represent the data of the object.
+
+        :return: The data of the object.
+        """
+
+        return {
+            self.TIME: self.time.json(),
+            self.INPUTS: self.inputs.json(),
+            self.OUTPUTS: self.outputs.json()
+        }
+    # end json
 # end Operation
--- looperator/process.py
+++ looperator/process.py
@@ -1,14 +1,55 @@
 # process.py
 
 import datetime as dt
+from typing import Dict, ClassVar, Any, Optional
 
 from attrs import define
+
+import pandas as pd
+import numpy as np
 
 from represent import represent
 
 __all__ = [
-    "ProcessTime"
+    "ProcessTime",
+    "to_datetime"
 ]
+
+def to_datetime(index: Any, adjust: Optional[bool] = True) -> dt.datetime:
+    """
+    Converts the index into a datetime object.
+
+    :param index: The value to convert.
+    :param adjust: The value to adjust the process for errors.
+
+    :return: The datetime object.
+    """
+
+    try:
+        if isinstance(index, str):
+            index = dt.datetime.fromisoformat(index)
+
+        elif isinstance(index, (int, float)):
+            index = dt.datetime.fromtimestamp(index)
+
+        elif isinstance(index, pd.Timestamp):
+            index = index.to_pydatetime()
+
+        elif isinstance(index, np.datetime64):
+            index = index.astype(dt.datetime)
+        # end if
+
+    except (TypeError, ValueError) as e:
+        if adjust:
+            pass
+
+        else:
+            raise e
+        # end if
+    # end try
+
+    return index
+# end to_datetime
 
 @represent
 @define(repr=False, frozen=True)
@@ -17,6 +58,16 @@ class ProcessTime:
 
     start: dt.datetime
     end: dt.datetime
+
+    START: ClassVar[str] = "start"
+    END: ClassVar[str] = "end"
+
+    try:
+        from typing import Self
+
+    except ImportError:
+        Self = Any
+    # end try
 
     @property
     def time(self) -> dt.timedelta:
@@ -28,4 +79,33 @@ class ProcessTime:
 
         return self.end - self.start
     # end time
+
+    @classmethod
+    def load(cls, data: Dict[str, float]) -> Self:
+        """
+        Creates an instance of the class for the data.
+
+        :param data: The data to load into an object.
+
+        :return: The new instance with the data.
+        """
+
+        return cls(
+            start=to_datetime(data[cls.START]),
+            end=to_datetime(data[cls.END])
+        )
+    # end load
+
+    def json(self) -> Dict[str, float]:
+        """
+        Returns a json object to represent the data of the object.
+
+        :return: The data of the object.
+        """
+
+        return {
+            self.START: self.start.timestamp(),
+            self.END: self.end.timestamp()
+        }
+    # end json
 # end ProcessTime
